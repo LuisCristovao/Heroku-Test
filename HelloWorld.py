1d9ad97382c03f6e702c7c2b01e1ad69bb52f8ae
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-05 15:09:10 +0100

    trying to publish in heroku

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,11 +7,14 @@ This is a temporary script file.
 import pandas as pd
 import numpy as np
 import bottle as bt
+import os
+from os import environ as env
+from sys import argv
 
 
 @bt.get('/') # or @route('/login')
 def init():
-    return '<h1>First Heroku App!!!</h1>'
+    return '''<h1>First Heroku App!!!</h1>'''
 
 
 bt.run(host='localhost', port=80, server='paste')
