d15473181b157020b310ea717714c694280834d6
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-05 15:31:54 +0100

    it worked but lets try something else

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,12 +4,12 @@
 import os
 from os import environ as env
 from sys import argv
-import bottle
-from bottle import default_app, request, route, response, get
+import bottle as bt
 
-@get('/') # or @route('/login')
+
+@bt.get('/') # or @route('/login')
 def init():
     return '''<h1>First Heroku App!!!</h1>'''
 
 
-bottle.run(host='0.0.0.0', port=argv[1])
+bt.run(host='0.0.0.0', port=argv[1])
