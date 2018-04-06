e4acd1d54008244267b31ccfc5bf3957a176b0c6
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-06 13:27:08 +0100

    trying to import own modules

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,15 +7,20 @@ from sys import argv
 import bottle as bt
 import pandas as pd
 import numpy as np
+import testmodule as test
+
+
+mt=test.Test('You are the visitor: ')
 
 counter=0
 
 @bt.get('/') # or @route('/login')
 def init():
+    global mt
     global counter
     counter+=1
     #return "<h1>First Heroku App "+str(np.random.random_sample())+" !!!</h1>"
-    return "<h1>Your are the visitor "+str(counter)+" !!!</h1>"
+    return "<h1>"+mt.speak()++str(counter)+" !!!</h1>"
 
 
 bt.run(host='0.0.0.0', port=argv[1], server='paste')
---
+++ testmodule.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 13:20:03 2018
+
+@author: lcristovao
+"""
+
+class Test:
+    
+    def __init__(self,text):
+        self.text=text
+        
+    def speak(self):
+        print(self.text)
+        
+        
+print(__name__)
