ffa16080a7c7906cc2ff19f9e1930b30ffe23ad0
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-06 09:46:52 +0100

    visit ciounter

--- HelloWorld.py
+++ HelloWorld.py
@@ -8,10 +8,13 @@ import bottle as bt
 import pandas as pd
 import numpy as np
 
+counter=0
+
 @bt.get('/') # or @route('/login')
 def init():
-	
-    return "<h1>First Heroku App "+str(np.random.random_sample())+" !!!</h1>"
+	counter+=1
+    #return "<h1>First Heroku App "+str(np.random.random_sample())+" !!!</h1>"
+    return "<h1>Your are the visitor "+str(counter)+" !!!</h1>"
 
 
 bt.run(host='0.0.0.0', port=argv[1])
